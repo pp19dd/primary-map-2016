fa0941c6e4a098027da27a8a6ba67b14df03de1e
pp19dd <pp19dd@gmail.com>
2016-04-29 15:43:25 -0400

    Edge

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $candidates = array(
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
     <link href="jqvmap.css" media="screen" rel="stylesheet" type="text/css">
 
 <style>
