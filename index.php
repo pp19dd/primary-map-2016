e1cbf1d8b99e380fbdafd9f808005f36a68448df
pp19dd <pp19dd@gmail.com>
2016-04-29 14:04:45 -0400

    Chart in tooltip

--- index.php
+++ index.php
@@ -29,6 +29,10 @@ margin: 0;
 width: 100%;
 height: 100%;
 }
+* {
+    font-family: Arial,Helvetica,sans-serif;
+    font-size: 15px;
+}
 #vmap {
 width: 100%;
 height: 100%;
@@ -36,18 +40,45 @@ background-color: red;
 -webkit-tap-highlight-color: rgba(0,0,0,0);
 }
 
-.candidate-name { text-align: center }
-.candidate-click { }
-.candidate-click-selected {};
+/* div version */
+.candidates { }
+.candidate { width: 20%; float: left; text-align: center; }
+.candidate-container { padding: 0.25em; }
+.candidate-inner { border-radius:0.5em; cursor: pointer; padding-top:0.5em; padding-bottom:0.5em; border: 4px solid #999 }
 
-.candidate-party-d.candidate-click-selected { background-color: #e91d0e !important; }
-.candidate-party-r.candidate-click-selected { background-color: #e91d0e !important; }
 
-td.candidate-name { }
-td.candidate-name a { color: black; font-size: 18px; }
-td.candidate-picture { }
-.candidate-delegate-count { text-align: center }
-td.candidate-delegate-count a { color: black; font-size: 32px; color:#333 }
+.candidate-inner { transition: background-color 100ms linear; }
+
+.candidate-party-d .candidate-inner { border: 4px solid #423bbf }
+.candidate-party-r .candidate-inner { border: 4px solid #e91d0e }
+
+.candidate-name { font-weight: bold }
+.candidate-name, .candidate-delegate-count-compute { color: #333 }
+/*
+.candidate-party-d .candidate-name, .candidate-party-d .candidate-delegate-count-compute { color: #232066 }
+.candidate-party-r .candidate-name, .candidate-party-r .candidate-delegate-count-compute { color: #e91d0e }
+*/
+.candidate-party-d.candidate-click-selected .candidate-inner { background-color: #232066; border: 4px solid #232066 }
+.candidate-party-r.candidate-click-selected .candidate-inner { background-color: #e91d0e; }
+
+.candidate-party-d.candidate-click-selected .candidate-name { color: white; }
+.candidate-party-r.candidate-click-selected .candidate-name { color: white; }
+
+.candidate-party-d.candidate-click-selected .candidate-delegate-count-compute { color: white; }
+.candidate-party-r.candidate-click-selected .candidate-delegate-count-compute { color: white; }
+
+.candidate-click-selected { }
+
+.candidates-note { }
+.candidates-note-all { font-size: 0.75em }
+.candidates-note-d { float: right; padding-right:2em; color: #232066 }
+.candidates-note-r { float: left; padding-left: 2em; color: #e91d0e }
+
+.tooltip { width: 100% }
+.tooltip * { font-size: 12px }
+.tooltip-name { width: 50px }
+
+.clearDiv { clear: both }
 </style>
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
@@ -71,8 +102,8 @@ jQuery(document).ready(function () {
 
             function highlight_candidate(candidate, color, party) {
 
-                $(".candidate-click").removeClass("candidate-click-selected");
-                $(".candidate-click.candidate-" + candidate).addClass("candidate-click-selected");
+                $(".candidate").removeClass("candidate-click-selected");
+                $(".candidate.candidate-" + candidate).addClass("candidate-click-selected");
 
                 for( var k in states )(function(code, state) {
 
@@ -152,8 +183,33 @@ jQuery(document).ready(function () {
 
                 // display totals
                 for( var d in counts )(function(candidate, delegate_count) {
-                    $("span.candidate-delegate-count-compute.candidate-" + candidate).html( delegate_count );
+                    $("div.candidate-delegate-count-compute.candidate-" + candidate).html( delegate_count );
                 })(d, counts[d]);
+            }
+
+            function html_count(count, max, color) {
+                var c_width = 100 * (parseInt(count) / parseInt(max));
+                var c_inverse_width = 100 - c_width;
+                return(
+                    "<table style='width: 100%'>" +
+                    "<tr>" +
+                    "<td style='background-color: " + color + "; width:" + c_width.toString() + "%'></td>" +
+                    "<td style='width:" + c_inverse_width + "%'>" + count + "</td>" +
+                    "</tr>" +
+                    "</table>"
+                );
+            }
+
+            function html_graph(data, color) {
+                var html = "<table class='tooltip'>";
+                for( var i = 0; i < data.length; i++ )(function(pt) {
+                    html += "<tr><td class='tooltip-name'>" + pt.name + "</td>";
+                    html += "<td>" + html_count(pt.count, pt.max, color) + "</td>";
+                    html += "</tr>";
+                })(data[i])
+                html += "</table>";
+
+                return( html );
             }
 
             // debug: did_candidate_win_state("trump", all_data[47], "r");
@@ -175,13 +231,17 @@ jQuery(document).ready(function () {
                     label.html(
                         "<strong>" + states[code].state + "</strong><br/><hr/>" +
                         "Republican Delegates: " + states[code].total_delegates_gop + "<br/>" +
-                        "Trump: " + states[code].trump + "<br/>" +
-                        "Cruz: " + states[code].cruz + "<br/>" +
-                        "Kasich: " + states[code].kasich + "<br/>" +
+                        html_graph([
+                            { max: states[code].total_delegates_gop, name: "Trump", count: states[code].trump },
+                            { max: states[code].total_delegates_gop, name: "Cruz", count: states[code].cruz },
+                            { max: states[code].total_delegates_gop, name: "Kasich", count: states[code].kasich }
+                        ], "#e91d0e") +
                         "<hr/>" +
                         "Democratic Delegates: " + states[code].total_delegates_dem + "<br/>" +
-                        "Clinton: " + states[code].clinton + "<br/>" +
-                        "Sanders: " + states[code].sanders + "<br/>"
+                        html_graph([
+                            { max: states[code].total_delegates_dem, name: "Clinton", count: states[code].clinton },
+                            { max: states[code].total_delegates_dem, name: "Sanders", count: states[code].sanders }
+                        ], "#423bbf")
                     );
                 },
                 onRegionClick: function(element, code, region) {
@@ -189,7 +249,7 @@ jQuery(document).ready(function () {
                 }
             });
 
-            $(".filter a").click(function() {
+            $(".filter div.candidate").click(function() {
                 var k = $(this).attr("data-key");
                 var c = $(this).attr("data-color");
                 var p = $(this).attr("data-party");
@@ -210,27 +270,26 @@ jQuery(document).ready(function () {
 
   <body>
     <div class="filter">
-<table style="width:100%">
-<tr>
+        <div class="candidates">
 <?php foreach( $candidates as $candidate ) { ?>
-    <td class="candidate-name">
-        <a class="candidate-click candidate-party-<?php echo strtolower($candidate["party"]); ?> candidate-<?php echo strtolower($candidate["name"]); ?>" data-party="<?php echo $candidate["party"] ?>" data-color="<?php echo $candidate["color"] ?>" data-key="<?php echo strtolower($candidate["name"]) ?>" href="#">
-            <?php echo $candidate["name"] ?>
-        </a>
-    </td>
+            <div class="candidate candidate-party-<?php echo strtolower($candidate["party"]); ?> candidate-<?php echo strtolower($candidate["name"]); ?>" data-party="<?php echo $candidate["party"] ?>" data-color="<?php echo $candidate["color"] ?>" data-key="<?php echo strtolower($candidate["name"]) ?>">
+                <div class="candidate-container">
+                    <div class="candidate-inner">
+                        <div class="candidate-name"><?php echo $candidate["name"] ?></div>
+                        <div class="candidate-delegate-count-compute candidate-<?php echo strtolower($candidate["name"]) ?>">0</div>
+                    </div>
+                </div>
+            </div>
 <?php } ?>
-</tr>
-<tr>
-<?php foreach( $candidates as $candidate ) { ?>
-    <td class="candidate-delegate-count">
-        <a class="candidate-click candidate-party-<?php echo strtolower($candidate["party"]); ?> candidate-<?php echo strtolower($candidate["name"]); ?>" data-party="<?php echo $candidate["party"] ?>" data-color="<?php echo $candidate["color"] ?>" data-key="<?php echo strtolower($candidate["name"]) ?>" href="#">
-            <span class="candidate-delegate-count-compute candidate-<?php echo strtolower($candidate["name"]) ?>">0</span>
-        </a>
-    </td>
-<?php } ?>
-</tr>
-</table>
+        </div>
+        <div class="clearDiv"></div>
+        <div class="candidates-note">
+            <div class="candidates-note-all candidates-note-r">Need 1,237 for nomination.</div>
+            <div class="candidates-note-all candidates-note-d">Need 2,283 for nomination.</div>
+        </div>
+        <div class="clearDiv"></div>
     </div>
+
     <div id="vmap"></div>
   </body>
 </html>
